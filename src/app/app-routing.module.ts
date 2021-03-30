ecf34465fbb799ef26796ae5eda91ff1c0b75d6c
Yurii On <onyshchak.com@gmail.com>
2021-03-30 15:38:08 +0300

    for deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+
 import {CountdownTimerComponent} from './start-page/countdown-timer/countdown-timer.component';
 
 
 const routes: Routes = [
-  // {
-  //   path: 'all',
-  //   loadChildren: () => import('./modules/all/all.module')
-  //     .then(mod => mod.AllModule)
-  // },
   {
     path: '**',
     component: CountdownTimerComponent
